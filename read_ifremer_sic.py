7f013a2b2df620b190699eaf9b92167baaee90fe
SLera <lera.selyzenok@gmail.com>
2017-07-05 16:29:03 +0300

    read ifremer nc, does not work

---
+++ read_ifremer_sic.py
@@ -0,0 +1,44 @@
+import numpy as np
+from netCDF4 import Dataset
+import matplotlib
+matplotlib.use('qt5agg')
+
+# def read_ifremer_sic_daily(FILENAME):
+#     """Returns U-,V-wind components as flaaten np.arrays for each grid cell in
+#     the for the SELS region (A) or central part of the SELS (B) or a grid point
+#     (C)
+#     CHECK THE REGION SETTINGS IN INSIDE THE FUNCTION
+#     Parameters:
+#     -----------
+#     year
+#     """
+#     upath = 'D:\\DATA\\NCEP1\\Wind_6h@10m\\UWind\\uwnd.10m.gauss.'
+#     vpath = 'D:\\DATA\\NCEP1\\Wind_6h@10m\\VWind\\vwnd.10m.gauss.'
+#     # get u-wind component and the grid
+#     f = Dataset(upath + str(year) + '.nc')
+#     time = f.variables['time'][:]
+#     lats = f.variables['lat'][:]
+#     lons = f.variables['lon'][:]
+#     uwind = f.variables['uwnd'][:]
+#     # get v-wind component (time, lats, long identical to u-wind vars)
+#     f = Dataset(vpath + str(year) + '.nc')
+#     vwind = f.variables['vwnd'][:]
+#     # A. slice data for the region based on lats_region and lons_region
+#     uwind_reg = uwind[:, 6:10, 67:75]
+#     vwind_reg = vwind[:, 6:10, 67:75]
+#     lats_reg = lats[6:10]
+#     lons_reg = lons[67:75]
+#
+#
+#     wtime = np.arange(0, (len(time) / 4) + 0.25, 0.25)
+#
+#     return uwind_reg.flatten(), vwind_reg.flatten(), lats_reg, lons_reg, wtime
+
+fname = '/home/valeria/NIERSC/Scripts/SIC_ocolor/test_data/19980101.nc'
+
+f = Dataset(fname)
+f.variables()
+sic = f.variables['concentration']
+
+OUTDIR = '/home/valeria/nfs0/data_sonarc/data/heap/ice_concentration/raw'
+
